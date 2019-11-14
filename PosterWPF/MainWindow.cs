e006713b600530040038737a7457c9c6b7b6e8da
KirillKutas <kutaskirillbstu@gmail.com>
2019-11-14 18:15:14 +0300

    First commit

---
+++ PosterWPF/MainWindow.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace PosterWPF
+{
+    partial class MainWindow
+    {
+        private void MoveMenu(double x1, double x2)
+        {
+            var animation = new ThicknessAnimation();
+            animation.From = new Thickness(x1, 10.091, 0, 0);
+            animation.To = new Thickness(x2, 10.091, 0, 0);
+            animation.SpeedRatio = 2;
+            Menu.BeginAnimation(MarginProperty, animation);
+        }
+    }
+}
---
+++ PosterWPF/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PosterWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        private void AnimationMenu_Click(object sender, RoutedEventArgs e)
+        {
+            if (Menu.Margin.Left != -248)
+            {
+                MoveMenu(Menu.Margin.Left, -248);
+            }
+            else
+            {
+                MoveMenu(-248, 0);
+            }
+        }
+
+        private void SearchButton_Click(object sender, RoutedEventArgs e)
+        {
+            MoveMenu(Menu.Margin.Left, -248);
+        }
+
+        private void FilmsButton_Click(object sender, RoutedEventArgs e)
+        {
+            MoveMenu(Menu.Margin.Left, -248);
+        }
+
+        private void ConcertsButton_Click(object sender, RoutedEventArgs e)
+        {
+            MoveMenu(Menu.Margin.Left, -248);
+        }
+
+        private void ExhibitionButton_Click(object sender, RoutedEventArgs e)
+        {
+            MoveMenu(Menu.Margin.Left, -248);
+        }
+
+        private void SettingsButton_Click(object sender, RoutedEventArgs e)
+        {
+            MoveMenu(Menu.Margin.Left, -248);
+        }
+    }
+}
